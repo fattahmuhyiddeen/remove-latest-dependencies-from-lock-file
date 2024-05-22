50f762ac9e9c6d84345ae9a59f7b177dad2b6187
Fattah Muhyiddeen <fattahmr@m1.com.sg>
2024-05-22 09:30:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Remove Latest Dependencies From Lock File
+
+This library is designed to be included in your project's `devDependencies` to automatically remove lock version in `package-lock.json` for all `dependencies` that use `latest` version.
+
+## How to use
+Just include this library in your `devDependecies` list.
---
+++ index.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
+const read = (filename) => fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
+
+try {
+  const { dependencies } = JSON.parse(read('package.json'));
+  const lockFile = JSON.parse(read('package-lock.json'));
+  for (const key in dependencies) {
+    if (dependencies[key] === 'latest') {
+      delete lockFile.packages['node_modules/' + key];
+    }
+  }
+  fs.writeFile('package-lock.json', JSON.stringify(lockFile, undefined, 2) + '\n', 'utf8', () => null);
+} catch (err) {
+  console.error('Error:', err);
+}
