912c888d337864259d369bf7a59ee6cc8ef1af37
Fattah Muhyiddeen <fattahmr@m1.com.sg>
2024-05-22 09:55:20 +0800

    fix path

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const fs = require('fs');
 
 const read = (filename) => fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
 
-fs.writeFile('test.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
-
 try {
-  const { dependencies } = JSON.parse(read('../package.json'));
-  const lockFile = JSON.parse(read('../package-lock.json'));
+  const { dependencies } = JSON.parse(read('../../package.json'));
+  const lockFile = JSON.parse(read('../../package-lock.json'));
   for (const key in dependencies) {
     if (dependencies[key] === 'latest') {
       delete lockFile.packages['node_modules/' + key];
     }
   }
-  fs.writeFile('../package-lock.json', JSON.stringify(lockFile, undefined, 2) + '\n', 'utf8', () => null);
+  fs.writeFile('../../package-lock.json', JSON.stringify(lockFile, undefined, 2) + '\n', 'utf8', () => null);
 } catch (err) {
-  console.error('Error:', err);
+  console.error(err);
 }
