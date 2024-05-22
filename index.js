2a465e5dbf64001368cf29e0a6ec4664221ea4cd
Fattah Muhyiddeen <fattahmr@m1.com.sg>
2024-05-22 10:07:37 +0800

    log

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ const fs = require('fs');
 
 const read = (filename) => fs.readFileSync(filename, { encoding: 'utf8', flag: 'r' });
 
-setTimeout(() => {
-  fs.writeFile('test.txt',  'hello', 'utf8', () => null);
-
-
 try {
   const { dependencies } = JSON.parse(read('../../package.json'));
   const lockFile = JSON.parse(read('../../package-lock.json'));
   for (const key in dependencies) {
     if (dependencies[key] === 'latest') {
       delete lockFile.packages['node_modules/' + key];
+      fs.appendFile('logs.txt',  key, 'utf8', () => null);
     }
   }
   fs.writeFile('../../package-lock.json', JSON.stringify(lockFile, undefined, 2) + '\n', 'utf8', () => null);
 } catch (err) {
   console.error(err);
 }
-}, 60000)
