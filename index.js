bc39a42b840a5a952a2a08a7b7c9dc27b5796816
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2025-03-28 08:19:02 +0800

    wrap under async

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
 
-try {
-  const read = (filename) =>
-    fs.readFileSync(filename, { encoding: "utf8", flag: "r" });
+const read = (filename) =>
+  fs.readFileSync(filename, { encoding: "utf8", flag: "r" });
+const main = async () => {
+  try {
+    const { dependencies, devDependencies } = JSON.parse(read("package.json"));
+    const lockFile = JSON.parse(read("package-lock.json"));
+    for (const key in dependencies) {
+      if (dependencies[key] === "latest") {
+        delete lockFile.packages["node_modules/" + key];
+      }
+    }
+    for (const key in devDependencies) {
+      if (devDependencies[key] === "latest") {
+        delete lockFile.packages["node_modules/" + key];
+      }
+    }
+    fs.writeFile(
+      "package-lock.json",
+      JSON.stringify(lockFile, undefined, 2) + "\n",
+      "utf8",
+      () => null
+    );
+  } catch (err) {
+    // console.log(err);
+  }
+};
 
-  const { dependencies, devDependencies } = JSON.parse(read("package.json"));
-  const lockFile = JSON.parse(read("package-lock.json"));
-  for (const key in dependencies) {
-    if (dependencies[key] === "latest") {
-      delete lockFile.packages["node_modules/" + key];
-    }
-  }
-  for (const key in devDependencies) {
-    if (devDependencies[key] === "latest") {
-      delete lockFile.packages["node_modules/" + key];
-    }
-  }
-  fs.writeFile(
-    "package-lock.json",
-    JSON.stringify(lockFile, undefined, 2) + "\n",
-    "utf8",
-    () => null
-  );
-} catch (err) {
-  console.log(err);
-}
+main();
